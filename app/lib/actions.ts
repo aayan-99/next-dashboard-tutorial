06b15a5beda11a8bbd6f6cd4d0dbd6e4d9c4d6d4
Aayanjit <aayanjit@ekoinfomatics.com>
2023-12-10 10:45:19 +0530

    Completed authentication and authorization functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 export type State = {
     errors?: {
@@ -79,12 +81,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
         };
     }
 
-    // const { customerId, amount, status } = UpdateInvoice.parse({
-    //     customerId: formData.get('customerId'),
-    //     amount: formData.get('amount'),
-    //     status: formData.get('status'),
-    // });
-
     const { customerId, amount, status } = validatedFields.data;
 
     const amountInCents = amount * 100;
@@ -109,4 +105,23 @@ export async function deleteInvoice(id: string) {
         return { message: 'Database Error: Failed to Delete Invoice.' }
     }
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
